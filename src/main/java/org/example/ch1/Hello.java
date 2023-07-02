04736de8ee0083e4a782f918186e53134ead99f3
Kim-Yong-Kyeong <kyk4957@naver.com>
2023-07-02 17:50:34 +0900

    ch1: print 출력

---
+++ src/main/java/org/example/ch1/Hello.java
@@ -0,0 +1,14 @@
+package org.example.ch1;
+
+public class Hello {
+	public static void main(String[] args) {
+		// ctrl + d : 행 단위 복사
+		// print()   - 출력 후 줄바꿈 안함
+		// println() - 출력 후 줄바꿈을 함
+		System.out.println("Hello, world");
+		System.out.println("Hello, world");
+		System.out.println("Hello, world");
+
+
+	}
+}
